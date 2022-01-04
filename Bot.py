3cbbb960088e93eec79c40616e4a4557d2847c6d
Denis <denis.shershakov.1995@gmail.com>
2022-01-04 22:29:12 +0300

    test bot

---
+++ Bot.py
@@ -0,0 +1,32 @@
+# Импорт комонентов
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+from settings import telegramApiUrl
+
+
+# функция будет вызвана при отправке команды start
+def sms(bot, update):
+    print('Отправили команду /start')
+    bot.message.reply_text('Приветствую, ' + format(bot.message.chat.first_name) + '!')
+    print(bot.message)
+    print(format(bot.message.chat.first_name))
+
+# функция parrot() отвечает пользователю его же сообщениями
+def parrot(bot, update):
+    #print(bot.message.text)
+    bot.message.reply_text(bot.message.text)
+
+# Соединение с платфоромой telrgram
+def main():
+    my_bot = Updater(telegramApiUrl)
+
+    my_bot.dispatcher.add_handler(CommandHandler('start', sms))
+
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.text, parrot))
+
+    my_bot.start_polling()
+    my_bot.idle()
+
+main()
